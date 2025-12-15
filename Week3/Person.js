0e13d5540d4825f1d2caaa0339a44b890621607e
anjalidawadi446-netizen <anjalidawadi446@gmail.com>
2025-12-15 02:42:29 +0000

    updated Week3

---
+++ Week3/Person.js
@@ -0,0 +1,31 @@
+class student {
+    constructor(name, age, email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    getPersonInfo() {
+        return {
+            Name: this.name,
+            Age: this.age,
+            Email: this.email
+        }
+    }
+}
+
+module.exports = student;
+
+//Exercise 4
+os=require("os")
+const util=require('util')
+console.log("temporary directory"+ os.tmpdir() )
+console.log("hostname: "+ os.hostname())
+console.log("OS : " + os.platform() +"release:"+ os.release())
+console.log("Uptime"+ (os.uptime())/3600 +" hours")
+console.log("userInfo" + util.inspect(os.userInfo()))
+console.log("Memory "+ os.totalmem()/1000000000 + "Giga byte")
+console.log(" free: "+os.freemem()/1000000000 + "Giga byte")
+console.log("CPU "+ util.inspect(os.cpus()))
+console.log("Network"+ util.inspect(os.networkInterfaces()))
+console.log("programe end")
