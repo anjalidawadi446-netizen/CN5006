792af0e3e4cab033310d2f723d88878680164f69
anjalidawadi446-netizen <anjalidawadi446@gmail.com>
2025-12-15 00:05:58 +0000

    Week 1 JavaScript program completed

---
+++ week1/index.js
@@ -0,0 +1,35 @@
+console.log("Hello");
+console.log("This is my First Programe");
+// Addition of two numbers
+const num1 = 5;
+const num2 = 4;
+const sum = num1 + num2;
+console.log("The Sum is " + sum);
+// Prompt input
+const prompt = require("prompt-sync")();
+console.log("Starting");
+// Get user's name
+const name = prompt("Enter your Name: ");
+console.log(`Hello, ${name}`);
+// Check positive, negative or zero
+const number = parseInt(prompt("Enter a number: "));
+if (number > 0) {
+  console.log("The number is Positive");
+} else if (number === 0) {
+  console.log("The Number is Zero");
+} else {
+  console.log("The number is Negative");
+}
+// Code for basic arithmetic operations
+const number1 = parseInt(prompt("Enter 1st number: "));
+const number2 = parseInt(prompt("Enter 2nd number: "));
+
+const sum2 = number1 + number2;
+const diff = number1 - number2;
+const mul = number1 * number2;
+const div = number1 / number2;
+
+console.log("The Sum of " + number1 + " and " + number2 + " is: " + sum2);
+console.log("The Difference of " + number1 + " and " + number2 + " is: " + diff);
+console.log("The Multiplication of " + number1 + " and " + number2 + " is: " + mul);
+console.log("The Division of " + number1 + " and " + number2 + " is: " + div);
